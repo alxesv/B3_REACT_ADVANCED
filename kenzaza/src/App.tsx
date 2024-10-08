3d5e63eed02d69748bc8597d220651498e5c5b26
sylv0r <sylvian.vidal95@gmail.com>
2024-10-08 16:23:17 +0200

    add router at app.tsx

--- kenzaza/src/App.tsx
+++ kenzaza/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MainPage from './pages/MainPage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <MainPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
