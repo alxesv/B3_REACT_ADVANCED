ee12d0922771a9cba3e4f6850a7d5aa04f9557a1
coding <coding@MacBook-Pro-de-coding.local>
2024-10-09 09:50:48 +0200

    Merge button page

--- kenzaza/src/App.tsx
+++ kenzaza/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import './App.css';
 import MainPage from './pages/MainPage';
 import CheckboxPage from './pages/CheckboxPage';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ButtonPage from './pages/button/ButtonPage';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/checkbox" element={<CheckboxPage />} />
+          <Route path="/button" element={<ButtonPage />} />
         </Routes>
       </Router>
     </div>
