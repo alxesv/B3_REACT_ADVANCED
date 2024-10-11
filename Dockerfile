55395834856c21f20c532a780feb090099941975
sylv0r <sylvian.vidal95@gmail.com>
2024-10-11 14:25:01 +0200

    mend

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,10 @@ WORKDIR /app
 
 COPY ./kenzaza/package.json ./
 RUN npm install
-
-COPY ./kenzaza .
-
-EXPOSE 3000
-
-CMD ["npm", "start"]
+COPY kenzaza/. .
+RUN npm run build
+# Production stage
+FROM nginx:stable-alpine as production-stage
+COPY --from=build-stage /app/kenzaza/build /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]""
--- kenzaza/src/components/atoms/Loader.test.tsx
+++ kenzaza/src/components/atoms/Loader.test.tsx
@@ -4,7 +4,7 @@ import Loader from './Loader';
 
 test('renders spinner loader by default', () => {
   render(<Loader type="spinner" />);
-  const spinner = screen.getByTestId('spinner'); 
+  const spinner = screen.getByTestId('spinner');
   expect(spinner).toHaveClass('spinner');
   expect(spinner).toHaveStyle('height: 10rem');
   expect(spinner).toHaveStyle('width: 10rem');
@@ -15,7 +15,7 @@ test('renders progress bar loader when type is progress', () => {
   render(<Loader type="progress" radius={10} width={50} height={2} colorType="secondary" />);
   const progressBar = screen.getByTestId('progress-bar');
   expect(progressBar).toHaveClass('progress-animation');
-  expect(progressBar).toHaveStyle('width: 50%'); 
-  expect(progressBar).toHaveStyle('height: 2rem'); 
+  expect(progressBar).toHaveStyle('width: 50%');
+  expect(progressBar).toHaveStyle('height: 2rem');
   expect(progressBar).toHaveClass('secondary');
 });
