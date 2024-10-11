4d878f05d12d59ff7462876b833d60fceb75a3a3
Kenza Tighrine <k.tghrn@outlook.fr>
2024-10-11 10:50:31 +0200

    mainpage

--- kenzaza/src/components/molecules/card/Card.tsx
+++ kenzaza/src/components/molecules/card/Card.tsx
@@ -7,9 +7,10 @@ interface CardProps {
     imageUrl?: string;
     actions?: React.ReactNode[];
     width?: string;
+    fontWeight?: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, description, imageUrl, actions, width=20 }) => {
+const Card: React.FC<CardProps> = ({ title, description, imageUrl, actions, width=20, fontWeight="lighter" }) => {
     return (
         <div className="card" style={{ width: `${width}rem` }}>
             {imageUrl && (
@@ -18,7 +19,8 @@ const Card: React.FC<CardProps> = ({ title, description, imageUrl, actions, widt
                 </div>
             )}
             <div className="card-content">
-                <h2 className="card-title">{title}</h2>
+                <h2 className="card-title" style={{ fontWeight: fontWeight }}>
+                  {title}</h2>
                 <p className="card-description">{description}</p>
             </div>
             {actions && (
